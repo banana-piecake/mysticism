40d4ea699fa4052467c4af78dc5b1410535e97a6
banana-piecake <pea0019@balwynhs.vic.edu.au>
2026-01-28 01:43:39 +0800

    intial commit
    initial commit

---
+++ src/client/java/mysticism/mysticism/client/MysticismClient.java
@@ -0,0 +1,10 @@
+package mysticism.mysticism.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MysticismClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/mysticism/mysticism/Mysticism.java
@@ -0,0 +1,10 @@
+package mysticism.mysticism;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mysticism implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
