a9e0bc54793ba45c3a253775e715ba5450db3b63
banana-piecake <pea0019@balwynhs.vic.edu.au>
2026-04-12 22:16:03 +1000

    added a new item called thunder orb which strikes lightning on all entties in range

--- src/main/java/mysticism/items/ModItems.java
+++ src/main/java/mysticism/items/ModItems.java
@@ -24,7 +24,7 @@ public class ModItems {
 
     public static final Item RUBY = registerItem("ruby", Item::new, new Item.Settings());
     public static final Item WINDORB = registerItem("wind_orb", DashItem::new, new Item.Settings().useCooldown(3));
-    public static final Item CREEPERTOTEM = registerItem("creeper_totem", CreeperTotemItem::new, new Item.Settings());
+    public static final Item THUNDERORB = registerItem("thunder_orb", DashItem::new, new Item.Settings().useCooldown(15));    public static final Item CREEPERTOTEM = registerItem("creeper_totem", CreeperTotemItem::new, new Item.Settings());
     public static final Item ENDORB = registerItem("mysterious_pearl", EnderItem::new, new Item.Settings().useCooldown(0.2f));
     public static final Item BAT = registerItem("bat", BatItem::new, new Item.Settings().sword(ToolMaterial.IRON,2.7f, -1.2f));
     public static final Item FELIXAXE = registerItem("felix_axe", FelixAxeItem::new, new Item.Settings().sword(ToolMaterial.IRON,3.7f, -1f));
---
+++ src/main/java/mysticism/items/ThunderItem.java
@@ -0,0 +1,44 @@
+package mysticism.items;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class ThunderItem extends Item {
+
+    public ThunderItem(Settings settings) {
+        super(settings);
+    }
+
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
+        Vec3d position = user.getEntityPos();
+        double radius = 6;
+        Box entitydetect =new Box(
+                position.x - radius, position.y - radius, position.z - radius,
+                position.x + radius, position.y + radius, position.z + radius
+        );
+        List<Entity> nearbyEntities = world.getOtherEntities(
+                user, // Exclude the user themselves
+                entitydetect,
+                (entity) -> entity instanceof Entity
+        );
+
+        LightningEntity lightning = EntityType.LIGHTNING_BOLT.create(world, SpawnReason.EVENT);
+        for (Entity entity : nearbyEntities) {
+            world.spawnEntity(lightning);
+        }
+        return ActionResult.SUCCESS;
+    }
+}
